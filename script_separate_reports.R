d953adb8fd7e6b503476c4d28b0869eb19c9e98a
Vladimir Bajić <44064386+bajicv@users.noreply.github.com>
2022-11-15 20:04:46 +0100

    Add files via upload

---
+++ script_separate_reports.R
@@ -0,0 +1,23 @@
+# libraries  -------------------------------------------------------------------
+library(tidyverse)
+library(rmarkdown)
+
+# data -------------------------------------------------------------------------
+data_file <- "Data_Di_Oral_Presentations_Peer-Review.tsv"
+df <- read.delim(data_file, check.names = F, as.is=T, encoding="UTF-8")
+
+# preparing loop ---------------------------------------------------------------
+to_loop <- df %>% 
+  select(BreakoutSession, Presenter) %>% 
+  unique()
+
+# loop -------------------------------------------------------------------------
+#for (i in 1:nrow(to_loop)) {
+for (i in 1:2) {
+  rmarkdown::render("Di_Peer-Review_Results.Rmd", 
+                    params = list(BreakoutSession = to_loop[i,1],
+                                  Presenter = to_loop[i,2],
+                                  data = data_file),
+                    output_file = paste0("Breakout_", to_loop[i,1], "_Presenter_", to_loop[i,2], ".html"))
+  
+}
